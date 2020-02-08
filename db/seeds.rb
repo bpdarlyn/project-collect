728f2e4b13a56cc4f198fc6b58d3199d73da89ee
Darlyn <bpdarlyn@gmail.com>
2020-02-08 17:26:31 -0400

    added

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+project = Project.create name: 'Retiro Espiritual 2020, Indetenibles',
+                         description: 'Indetenibles',
+                         start_date: Time.zone.now, end_date: Time.zone.now,
+                         active: true
+
+file_items_name = 'presupuesto-2020-retiro.xlsx'
+
+full_path = Rails.root.join('db', 'data', file_items_name)
+
+excel_file = Creek::Book.new full_path
+sheet = excel_file.sheets[0]
+columns = { qty: 'A', description: 'B', name: 'C', unit_price: 'D', total_amount: 'E' }
+sheet.simple_rows.each do |row|
+  next if row[columns[:qty]] === 'cant' # check if is the first row to skip the header
+  next if !row.present?
+  new_item = {
+      name: row[columns[:name]],
+      description: row[columns[:description]],
+      unit_price: row[columns[:unit_price]].to_f,
+      qty: row[columns[:qty]].to_i,
+      total_amount: row[columns[:total_amount]].to_f,
+      project: project,
+      current_state: 'pending' # collected, # inactive, # down
+  }
+  ItemCollect.create(new_item)
+end
+project.total_amount = ItemCollect.sum(:total_amount)
+project.save
