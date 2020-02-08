48133f85c33846e13701eda9f47b9ae829e7d919
Darlyn <bpdarlyn@gmail.com>
2020-02-08 15:24:04 -0400

    ready

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,16 +2,10 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
+// styles
+import './../main-app/application.scss'; // import styles
 
-require("@rails/ujs").start()
-require("turbolinks").start()
-require("@rails/activestorage").start()
-require("channels")
+require('./../main-app/js/application'); // import javascripts
 
-
-// Uncomment to copy all static images under ../images to the output folder and reference
+require.context('../images', true); // import all static images
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
-// or the `imagePath` JavaScript helper below.
-//
-// const images = require.context('../images', true)
-// const imagePath = (name) => images(name, true)
